17101a9757fe78a4b8e9bdc2f27977d97fd1abbd
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 10:13:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,15 @@
 function setup() {
     createCanvas(800, 800);
-    background("blue");
-}
+    background("yellow");
+    // p5 Color object
+    fill(color(20, 20, 60,60));
+    rect(20, 20, 60, 60);
+    describe('blue rect with black outline in center of canvas');
+    circle(400,300,300);
+    circle(300,500,300);
+    circle(500,500,300);
+    rect(100,100,100,100);
+    triangle(100, 100, 200, 100, 150, 50)
+    }
+    
+    
