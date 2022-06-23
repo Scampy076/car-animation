7ed46d963262545294082c8b8d43028d0eb0e520
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 10:16:57 +0200

    Mercedes AMG GT3

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(1200, 800);
     background("yellow");
-    // p5 Color object
-    fill(color(20, 20, 60,60));
-    rect(20, 20, 60, 60);
-    describe('blue rect with black outline in center of canvas');
-    circle(400,300,300);
-    circle(300,500,300);
-    circle(500,500,300);
-    rect(100,100,100,100);
-    triangle(100, 100, 200, 100, 150, 50)
-    }
-    
-    
+fill(color(0, 200, 500, 500));
+rect(200, 520, 700, 110);
+circle(300,600,100);
+circle(800,600,100);
+triangle(200, 450, 300, 450, 250, 400)
+arc(500, 525, 300, 200, PI, TWO_PI);
+line(300, 520, 275, 450)
+line(250, 520, 225, 450)
+line(250, 520, 275, 450)
+line(300, 520, 225, 450)
+let img = createImage(740, 605);
+img.loadPixels();
+for (let i = 327; i < img.width; i++) {
+  for (let j = 595; j < img.height; j++) {
+    img.set(i, j, color(255, 204, 0));
+  }
+}
+img.updatePixels();
+image(img, 17, 17);
+
+}
