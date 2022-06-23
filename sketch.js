ee2fb4864a4c0495a1048e9d7beaa69bcd0a612d
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 10:34:46 +0200

    paar foutjes bij de y code en x code, moet nog gefixed worden

--- sketch.js
+++ sketch.js
@@ -1,83 +1,89 @@
 function setup() {
   createCanvas(1200, 800);
-  background("yellow");
-  fill('grey')
-  rect(195, 623, 55, 15, 5, 5, 15, 5);
-describe('white rect with black outline and round edges of different radii');
-fill(color(0, 200, 500, 500));
-rect(200, 520, 700, 110, 80, 80, 3, 3);
-describe('white rect with black outline and round edges of different radii');
-triangle(200, 450, 300, 450, 250, 400)
-triangle(100, 400, 300, 450, 200, 450)
-arc(500, 525, 300, 200, PI, TWO_PI);
-line(300, 520, 275, 450)
-line(250, 520, 225, 450)
-triangle(xPos - 100, 400, xPos + 100, 450, xPos, 450)
-line(xPos + 50, 520, xPos + 75, 450)
-line(xPos + 100, 520, xPos + 25, 450)
+ background("yellow");
 
-let img = createImage(740, 605);
-img.loadPixels();
-for (let i = 327; i < img.width; i++) {
-@@ -23,57 +25,61 @@ for (let i = 327; i < img.width; i++) {
- }
-}
-fill('white')
-
-img.updatePixels();
-image(img, 17, 17);
-rect(xPos + 310, 440, 90, 80, 15, 80, 5, 5);
-rect(xPos * 2, 440, 90, 80, 80, 15, 5, 5);
-
-fill('red')
-
-rect(xPos + 15, 530, 55, 15, 50, 15, 10, 5);
-
-fill('yellow')
-
-rect(xPos + 625, 530, 55, 15, 5, 50, 15, 10)
-
-fill('black')
-
-circle(xPos + 100,600,100);
-circle(xPos * 4,600,100);
-rect(xPos + 655, 560, 45, 50, 5, 10, 5, 5)
-
-fill('gray')
-let offset = 655
- rect(xPos + offset, 560, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 565, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 570, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 575, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 580, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 585, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 590, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 595, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 600, 45, 3, 5, 10, 5, 5)
- rect(xPos + offset, 605, 45, 3, 5, 10, 5, 5)
- circle(xPos + 100, 600, 90);
- circle(xPos + 600, 600, 90);
- line(xPos + 300, 612, 500, 426);
- line(xPos + 450, 612, xPos + 450, 526);
-
-fill('black')
-
-circle(xPos + 100, 600, 20);
-circle(xPos + 600, 600, 20);
+let xPos = 200
+let yPos = 600
 
 fill('grey')
 
-rect(xPos + 305, 526, 30, 10, 20);
-triangle(xPos + 98, 600, xPos + 100, 590,xPos + 102, 600);
-triangle(xPos + 102, 599, xPos + 100, 601, xPos + 109, 606);
-triangle(xPos + 98, 600, xPos + 101, 601, xPos + 93, 606);
-triangle(xPos + 598, 600, xPos + 600, 590, xPos + 602, 600);
-triangle(xPos + 602, 599, xPos + 600, 601, xPos + 609, 606);
-triangle(xPos + 598, 600, xPos + 601, 601, xPos + 593, 606);
+ rect(xPos - 5, yPos + 23, 55, 15, 5, 5, 15, 5);
+
+fill(color(0, 200, 500, 500));
+
+let offset1 = -80
+let offset2 = -150
+ rect(xPos, yPos - 80, 700, 110, 80, 80, 3, 3);
+ triangle(xPos, yPos - 150, xPos + 100, yPos - 150, xPos + 50, yPos - 200)
+ triangle(xPos - 100, yPos - 200, xPos + 100, yPos - 150, xPos, yPos - 150)
+ arc(xPos + 300, yPos - 75, 300, 200, PI, TWO_PI);
+ line(xPos + 100, yPos + offset1, xPos + 75, yPos + offset2)
+ line(xPos + 50, yPos + offset1, xPos + 25, yPos + offset2)
+ line(xPos + 50, yPos + offset1, xPos + 75, yPos + offset2)
+ line(xPos + 100, yPos + offset1, xPos + 25, yPos + offset2)
+
+let img = createImage(xPos + 540, yPos + 5);
+img.loadPixels();
+for (let i = 327; i < img.width; i++) {
+  for (let j = 595; j < img.height; j++) {
+@@ -28,58 +34,59 @@ fill('white')
+
+ img.updatePixels();
+ image(img, 17, 17);
+  
+fill('red')
+
+rect(xPos + 15, yPos - 70, 55, 15, 50, 15, 10, 5);
+
+fill('yellow')
+
+rect(xPos + 625, yPos - 70, 55, 15, 5, 50, 15, 10)
+
+
+fill('black')
+
+circle(xPos + 100, yPos, 100);
+ circle(xPos * 4, yPos, 100);
+ rect(xPos + 655, yPos - 40, 45, 50, 5, 10, 5, 5)
+
+fill('gray')
+
+let offset = 655
+
+rect(xPos + offset, yPos - 40, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 35, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 30, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 25, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 20, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 15, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 10, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos - 5, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos, 45, 3, 5, 10, 5, 5)
+ rect(xPos + offset, yPos + 5, 45, 3, 5, 10, 5, 5)
+ circle(xPos + 100, yPos, 90);
+ circle(xPos + 600, yPos, 90);
+ line(xPos + 300, yPos + 12, xPos + 300, yPos - 174);
+ line(xPos + 450, yPos + 12, xPos + 450, yPos - 74);
+
+fill('black')
+
+circle(xPos + 100, yPos, 20);
+ circle(xPos + 600, yPos, 20);
+
+fill('grey')
+
+ rect(xPos + 305, 526, 30, 10, 20);
+
+ triangle(xPos + 98, yPos, xPos + 100, yPos - 10,xPos + 102, yPos);
+ triangle(xPos + 102, yPos - 1, xPos + 100, yPos + 1, xPos + 109, yPos + 6);
+ triangle(xPos + 98, yPos, xPos + 101, yPos + 1, xPos + 93, yPos + 6);
+ triangle(xPos + 598, yPos, xPos + 600, yPos - 10, xPos + 602, yPos);
+ triangle(xPos + 602, yPos - 1, xPos + 600, yPos + 1, xPos + 609, yPos + 6);
+ triangle(xPos + 598, yPos, xPos + 601, yPos + 1, xPos + 593, yPos + 6);
 
 fill(0,90)
 
-circle(xPos - 15, 620, 30)
-circle(xPos - 50, 610, 50)
-circle(xPos - 95, 580, 70)
+circle(xPos - 15, yPos + 20, 30)
+ circle(xPos - 50, yPos + 10, 50)
+ circle(xPos - 95, yPos - 20, 70)
 }
