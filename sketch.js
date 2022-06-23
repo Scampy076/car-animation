68e89b5c76c941a61c4197181f0add053e45931d
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 10:25:31 +0200

    afgemaakte mercedes benz amg gt3

--- sketch.js
+++ sketch.js
@@ -1,24 +1,72 @@
 function setup() {
-    createCanvas(1200, 800);
-    background("yellow");
-fill(color(0, 200, 500, 500));
-rect(200, 520, 700, 110);
+  createCanvas(1200, 800);
+  background("yellow");
+  fill('grey')
+ rect(200, 520, 700, 110);
 circle(300,600,100);
 circle(800,600,100);
+rect(200, 520, 700, 110, 80, 80, 3, 3);
+describe('white rect with black outline and round edges of different radii');
 triangle(200, 450, 300, 450, 250, 400)
+triangle(100, 400, 300, 450, 200, 450)
 arc(500, 525, 300, 200, PI, TWO_PI);
 line(300, 520, 275, 450)
 line(250, 520, 225, 450)
-line(250, 520, 275, 450)
-line(300, 520, 225, 450)
-let img = createImage(740, 605);
-img.loadPixels();
-for (let i = 327; i < img.width; i++) {
-  for (let j = 595; j < img.height; j++) {
-    img.set(i, j, color(255, 204, 0));
-  }
+@@ -19,9 +22,58 @@ for (let i = 327; i < img.width; i++) {
 }
+}
+fill('white')
 img.updatePixels();
 image(img, 17, 17);
-
+rect(510, 440, 90, 80, 15, 80, 5, 5);
+describe('white rect with black outline and round edges of different radii');
+rect(400, 440, 90, 80, 80, 15, 5, 5);
+describe('white rect with black outline and round edges of different radii');
+fill('red')
+rect(215, 530, 55, 15, 50, 15, 10, 5);
+describe('white rect with black outline and round edges of different radii');
+fill('yellow')
+rect(825, 530, 55, 15, 5, 50, 15, 10)
+describe('white rect with black outline and round edges of different radii');
+fill('black')
+circle(300,600,100);
+circle(800,600,100);
+rect(855, 560, 45, 50, 5, 10, 5, 5)
+fill('gray')
+rect(855, 560, 45, 3, 5, 10, 5, 5)
+rect(855, 565, 45, 3, 5, 10, 5, 5)
+rect(855, 570, 45, 3, 5, 10, 5, 5)
+rect(855, 575, 45, 3, 5, 10, 5, 5)
+rect(855, 580, 45, 3, 5, 10, 5, 5)
+rect(855, 585, 45, 3, 5, 10, 5, 5)
+rect(855, 590, 45, 3, 5, 10, 5, 5)
+rect(855, 595, 45, 3, 5, 10, 5, 5)
+rect(855, 600, 45, 3, 5, 10, 5, 5)
+rect(855, 605, 45, 3, 5, 10, 5, 5)
+circle(300,600,90);
+circle(800,600,90);
+line(500, 612, 500, 426);
+line(650, 612, 650, 526);
+fill('black')
+circle(300,600,20);
+circle(800,600,20);
+fill('grey')
+rect(505, 526, 30, 10, 20);
+describe('white rect with black outline and round edges in mid-right of canvas');
+triangle(298, 600, 300, 590, 302, 600);
+describe('white triangle with black outline in mid-right of canvas');
+triangle(302, 599, 300, 601, 309, 606);
+describe('white triangle with black outline in mid-right of canvas');
+triangle(298, 600, 301, 601, 293, 606);
+describe('white triangle with black outline in mid-right of canvas');
+triangle(798, 600, 800, 590, 802, 600);
+describe('white triangle with black outline in mid-right of canvas');
+triangle(802, 599, 800, 601, 809, 606);
+describe('white triangle with black outline in mid-right of canvas');
+triangle(798, 600, 801, 601, 793, 606);
+describe('white triangle with black outline in mid-right of canvas');
+fill(0,90)
+circle(185,620,30)
+circle(150,610,50)
+circle(105,580,70)
 }
